6509c2dec3c9418cc375247d663044af14ea2900
dhruva-itest <dhruva.upadhyaya@yugabyte.com>
2025-10-28 15:18:39 +0530

    Added research paper get_important_keywords() function

--- research_paper_helper/test_rp_utils.py
+++ research_paper_helper/test_rp_utils.py
@@ -1,6 +1,13 @@
 import arxiv
 import os
 from os.path import isfile, join
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import re
+from collections import Counter
+import yake
+from sentence_transformers import SentenceTransformer, util
+
 
 def download_research_paper_for_topic(topic_name: str):
     research_paper = topic_name
@@ -29,3 +36,35 @@ def get_file_names(topic_name):
     return files
 
 # print(get_file_names("RLHF"))
+embedder = SentenceTransformer('all-MiniLM-L6-v2')
+def get_important_keywords(research_paper):
+    import sys
+    import os
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    from rag.data_parser import PDFReader
+
+    text = PDFReader(pdf_paths=[research_paper]).read()
+    text = " ".join(text)
+    
+    kw_extractor = yake.KeywordExtractor(
+        lan="en",
+        n=3,
+        dedupLim=0.9,
+        dedupFunc='seqm',
+        windowsSize=1,
+        top=100,
+        features=None
+    )
+    yake_keywords = [kw for kw, antiscore in kw_extractor.extract_keywords(text)]
+    title = " ".join(research_paper.split("/")[-1].replace("_draft.pdf", "").split("_")).lower()
+    # print(title)
+    title_emb = embedder.encode(title, convert_to_tensor=True)
+    kw_embs = embedder.encode(yake_keywords, convert_to_tensor=True)
+    sims = util.cos_sim(title_emb, kw_embs)[0]
+    ranked = sorted(zip(yake_keywords, sims.tolist()), key=lambda x: x[1], reverse=True)
+    ranked = [k for k, score in ranked if score > 0.2]
+    return ranked
+
+# TODO - the title always has to be perfect otherwise it just won't work
+# because it is a draft research paper, we can assume the person uploading it will give the perfect title
+print(get_important_keywords("./documents/Reinforcement_Learning_from_Human_Feedback_draft.pdf"))
